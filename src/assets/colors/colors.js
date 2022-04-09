3200daf42412c22986732d380a91d4d38aeda846
sommykene <32955277+sommykene@users.noreply.github.com>
2022-04-09 15:57:18 +0100

    adding colour values

---
+++ src/assets/colors/colors.js
@@ -0,0 +1,15 @@
+export const color = {
+  mono: {
+    white: "#FFFFFF",
+    lightgrey: "#DBDBDB",
+    darkgrey: "#707070",
+    black: "#000000",
+  },
+  background: "#E9EDF0",
+  duckegg: "#9EDBD7",
+  teal: "#4E9B95",
+  yellow: "#FFD62F",
+  orange: "#FF8801",
+  pink: "#D12E2E",
+  green: "#008751",
+};
