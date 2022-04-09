2fb3574ee409845eeac7db683312b5c27592f4dc
sommykene <32955277+sommykene@users.noreply.github.com>
2022-04-09 15:58:11 +0100

    added language toggle component

---
+++ src/components/ToggleSwitch.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import { color } from "../assets/colors/colors";
+
+function ToggleSwitch() {
+  const [language, setLanguage] = useState({
+    en: true,
+    enig: false,
+    ig: false,
+  });
+
+  return (
+    <div style={styles.container}>
+      <div style={styles.toggle}>
+        <span style={styles.dot}></span>
+        <span style={styles.active}></span>
+        <span style={styles.dot}></span>
+      </div>
+      <div style={styles.lang}>
+        <span>EN</span>
+        <span>EN/IG</span>
+        <span>IG</span>
+      </div>
+    </div>
+  );
+}
+
+const styles = {
+  container: {
+    width: "50%",
+  },
+  toggle: {
+    border: `3px ${color.duckegg} solid`,
+    borderRadius: "50px",
+    padding: "5px",
+    display: "flex",
+    justifyContent: "space-between",
+  },
+  dot: {
+    height: "15px",
+    width: "15px",
+    border: `1px ${color.mono.black} solid`,
+    borderRadius: "50%",
+    display: "inline-block",
+  },
+  active: {
+    height: "15px",
+    width: "15px",
+    borderRadius: "50%",
+    display: "inline-block",
+    backgroundColor: color.mono.black,
+  },
+  lang: {
+    display: "flex",
+    justifyContent: "space-between",
+    color: color.teal,
+    paddingLeft: "6px",
+    paddingRight: "6px",
+  },
+};
+
+export default ToggleSwitch;
