4ea1b77d0dd78189a71584304fcac4de00448b3f
sommykene <32955277+sommykene@users.noreply.github.com>
2022-04-09 17:42:40 +0100

    remove in file component functions

--- src/screens/home/HomeScreen.js
+++ src/screens/home/HomeScreen.js
@@ -8,92 +8,75 @@ import CultureIcon from "../../assets/images/icons/culture_icon_large.png";
 
 import { color } from "../../assets/colors/colors";
 import "./HomeScreen.css";
+import WordOfTheDayCard from "./componenets/WordOfTheDayCard";
 
 function HomeScreen() {
   return (
     <div id="homescreen">
+      {/* ROWONE */}
       <div style={{ marginBottom: "30px" }} className="rowone">
+        {/* Profile */}
         <div style={{ flex: 1 }}>
           <ProfileCard />
         </div>
 
+        {/* learn */}
         <div style={{ flex: 2 }}>
-          <LearnCard />
+          <div
+            style={{ backgroundColor: color.teal }}
+            className="section-cards"
+          >
+            <img style={{ height: "150px", width: "150px" }} src={LearnIcon} />
+            <span style={{ fontSize: "24pt", color: "white" }}>Learn</span>
+          </div>
         </div>
 
+        {/* Practice */}
         <div style={{ flex: 2 }}>
-          <PracticeCard />
+          <div
+            style={{ backgroundColor: color.duckegg }}
+            className="section-cards"
+          >
+            <img
+              style={{ height: "150px", width: "150px" }}
+              src={PracticeIcon}
+            />
+            <span style={{ fontSize: "24pt" }}>Practice</span>
+          </div>
         </div>
       </div>
 
+      {/* ROWTWO */}
       <div className="rowtwo">
+        {/* vocabulary */}
         <div style={{ flex: 2 }}>
-          <VocabCard />
+          <div
+            style={{ backgroundColor: color.yellow }}
+            className="section-cards"
+          >
+            <img style={{ height: "150px", width: "150px" }} src={VocabIcon} />
+            <span style={{ fontSize: "24pt" }}>Vocab</span>
+          </div>
         </div>
 
-        <div style={{ flex: 1 }}>
-          <WotdCard />
+        {/* word of the day */}
+        <div style={{ flex: 1.5 }}>
+          <WordOfTheDayCard />
         </div>
 
+        {/* culture */}
         <div style={{ flex: 2 }}>
-          <CultureCard />
+          <div style={{ backgroundColor: color.red }} className="section-cards">
+            <img
+              style={{ height: "150px", width: "150px" }}
+              src={CultureIcon}
+            />
+            <span style={{ fontSize: "24pt", color: "white" }}>Culture</span>
+          </div>
         </div>
       </div>
     </div>
   );
 }
-
-const LearnCard = () => {
-  return (
-    <div style={{ backgroundColor: color.teal }} className="section-cards">
-      <img style={{ height: "150px", width: "150px" }} src={LearnIcon} />
-      <span style={{ fontSize: "24pt", color: "white" }}>Learn</span>
-    </div>
-  );
-};
-
-const PracticeCard = () => {
-  return (
-    <div style={{ backgroundColor: color.duckegg }} className="section-cards">
-      <img style={{ height: "150px", width: "150px" }} src={PracticeIcon} />
-      <span style={{ fontSize: "24pt" }}>Practice</span>
-    </div>
-  );
-};
-
-const VocabCard = () => {
-  return (
-    <div style={{ backgroundColor: color.yellow }} className="section-cards">
-      <img style={{ height: "150px", width: "150px" }} src={VocabIcon} />
-      <span style={{ fontSize: "24pt" }}>Vocab</span>
-    </div>
-  );
-};
-
-const WotdCard = () => {
-  return (
-    <div style={styles.wotd}>
-      <span>WOTD</span>
-    </div>
-  );
-};
-
-const CultureCard = () => {
-  return (
-    <div style={{ backgroundColor: color.red }} className="section-cards">
-      <img style={{ height: "150px", width: "150px" }} src={CultureIcon} />
-      <span style={{ fontSize: "24pt", color: "white" }}>Culture</span>
-    </div>
-  );
-};
-
-const styles = {
-  wotd: {
-    border: `3px ${color.orange} solid`,
-    backgroundColor: color.white,
-    borderRadius: "10px",
-    height: "350px",
-  },
-};
 
 export default HomeScreen;
