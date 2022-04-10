ebd6b02950794a522cdc8a18ae10a7e1751392cd
sommykene <32955277+sommykene@users.noreply.github.com>
2022-04-10 10:54:16 +0100

    install react spotify embed

--- src/screens/culture/CultureHomeScreen.js
+++ src/screens/culture/CultureHomeScreen.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { useTranslation } from "react-i18next";
+import Spotify from "react-spotify-embed";
 import { color } from "../../assets/colors/colors";
 import BottomColorStrip from "../../components/BottomColorStrip";
 import Sidebar from "../../components/Sidebar";
@@ -31,14 +32,10 @@ function CultureHomeScreen() {
           <p style={{ marginTop: "20px" }} className="balsamiq-ig subtitle">
             Igbo Music Playlist
           </p>
-          <iframe
-            style={{ borderRadius: "12px" }}
-            src="https://open.spotify.com/embed/playlist/6OEAko3c1E6ageXECJpIK4?utm_source=generator"
-            width="100%"
-            height="380"
-            frameBorder="0"
-            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-          ></iframe>
+          <Spotify
+            wide
+            link="https://open.spotify.com/track/5ihDGnhQgMA0F0tk9fNLlA?si=4472348a63dd4f83"
+          />
         </div>
       </div>
     </div>
