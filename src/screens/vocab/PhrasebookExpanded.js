36ef0d9d7b14894415405ed7ada0ad5c0eb512be
sommykene <32955277+sommykene@users.noreply.github.com>
2022-04-10 19:00:39 +0100

    added layout and back button

--- src/screens/vocab/PhrasebookExpanded.js
+++ src/screens/vocab/PhrasebookExpanded.js
@@ -1,14 +1,35 @@
 import React from "react";
 import Sidebar from "../../components/Sidebar";
 import BottomColorStrip from "../../components/BottomColorStrip";
+import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router-dom";
+import { icon } from "../../assets/images";
+import { color } from "../../assets/colors/colors";
 
 function PhrasebookExpanded() {
   const page = "vocab";
+  const { t, i18n } = useTranslation("common");
+  const navigate = useNavigate();
+
   return (
     <div style={{ display: "flex", gap: "30px" }}>
       <div style={{ flex: 1 }}>
         <Sidebar page={page} />
         <BottomColorStrip page={page} />
+      </div>
+
+      {/* MAIN BODY */}
+      <div style={{ flex: 4 }}>
+        <div>
+          <img
+            style={{ marginRight: "10px", cursor: "pointer" }}
+            src={icon.backarrow}
+            onClick={() => navigate("/vocab")}
+          />
+          <span className="pagetitle balsamiq-ig">{t("vocab")}</span>
+        </div>
+
+        <p className="subtitle balsamiq-ig">Phrasebooks</p>
       </div>
     </div>
   );
