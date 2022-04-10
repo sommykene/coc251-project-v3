5aaadba3f9f8d8acc22781a6b8b7e26ffa58bfbb
sommykene <32955277+sommykene@users.noreply.github.com>
2022-04-10 08:02:04 +0100

    fixed and ontop

--- src/components/BottomColorStrip.js
+++ src/components/BottomColorStrip.js
@@ -24,12 +24,13 @@ function BottomColorStrip({ page }) {
   return (
     <div
       style={{
-        position: "absolute",
+        position: "fixed",
         bottom: 0,
         left: 0,
         height: "30px",
         width: "100%",
         backgroundColor: stripColor,
+        zIndex: 1,
       }}
     ></div>
   );
