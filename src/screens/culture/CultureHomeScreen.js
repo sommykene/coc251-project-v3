e048c8fc4e7207eb3495722ec88b4b12527fc1e8
sommykene <32955277+sommykene@users.noreply.github.com>
2022-04-10 17:37:56 +0100

    removed react spotify embed + reorder

--- src/screens/culture/CultureHomeScreen.js
+++ src/screens/culture/CultureHomeScreen.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react";
 import { useTranslation } from "react-i18next";
-import Spotify from "react-spotify-embed";
 import { color } from "../../assets/colors/colors";
 import BottomColorStrip from "../../components/BottomColorStrip";
 import Sidebar from "../../components/Sidebar";
@@ -20,7 +19,12 @@ function CultureHomeScreen() {
       <div style={{ flex: 4 }}>
         <span className="pagetitle balsamiq-ig">{t("culture")}</span>
         <div style={styles.main}>
-          <p className="balsamiq-ig subtitle">Igbo Music Playlist</p>
+          <p className="balsamiq-ig subtitle">
+            Igbo Infographics (In Partnership with OkwuID)
+          </p>
+          <p style={{ marginTop: "20px" }} className="balsamiq-ig subtitle">
+            Igbo Music Spotify Playlist
+          </p>
           <iframe
             style={{ borderRadius: "12px" }}
             src="https://open.spotify.com/embed/playlist/6OEAko3c1E6ageXECJpIK4?utm_source=generator"
@@ -29,13 +33,6 @@ function CultureHomeScreen() {
             frameBorder="0"
             allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
           ></iframe>
-          <p style={{ marginTop: "20px" }} className="balsamiq-ig subtitle">
-            Igbo Music Playlist
-          </p>
-          <Spotify
-            wide
-            link="https://open.spotify.com/track/5ihDGnhQgMA0F0tk9fNLlA?si=4472348a63dd4f83"
-          />
         </div>
       </div>
     </div>
