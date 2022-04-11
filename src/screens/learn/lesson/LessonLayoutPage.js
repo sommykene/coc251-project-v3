905ce120c045a6c2a2ead92efd37880c2c368a9f
sommykene <32955277+sommykene@users.noreply.github.com>
2022-04-11 11:31:46 +0100

    added link to leave button

--- src/screens/learn/lesson/LessonLayoutPage.js
+++ src/screens/learn/lesson/LessonLayoutPage.js
@@ -78,25 +78,27 @@ const LeaveButton = () => {
   }, []);
 
   return (
-    <div
-      className="balsamiq-ig"
-      style={{
-        position: "absolute",
-        bottom: "50px",
-        right: "50px",
-        width: "fit-content",
-        backgroundColor: color.red,
-        color: color.white,
-        padding: "10px",
-        borderRadius: "10px",
-        display: "flex",
-        justifyContent: "center",
-        gap: "10px",
-      }}
-    >
-      <img style={{ height: "20px" }} src={icon.backarrow.white} />
-      <span>{t("leave")}</span>
-    </div>
+    <Link to="/learn/1/details?tab=viewlessons">
+      <div
+        className="balsamiq-ig"
+        style={{
+          position: "absolute",
+          bottom: "50px",
+          right: "50px",
+          width: "fit-content",
+          backgroundColor: color.red,
+          color: color.white,
+          padding: "10px",
+          borderRadius: "10px",
+          display: "flex",
+          justifyContent: "center",
+          gap: "10px",
+        }}
+      >
+        <img style={{ height: "20px" }} src={icon.backarrow.white} />
+        <span>{t("leave")}</span>
+      </div>
+    </Link>
   );
 };
 
