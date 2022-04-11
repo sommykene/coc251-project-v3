98bbbcc02c97e4e198fe2b35bfe83df890ce90b2
sommykene <32955277+sommykene@users.noreply.github.com>
2022-04-11 08:50:45 +0100

    translation grid extracted

---
+++ src/components/TranslationsGrid.js
@@ -0,0 +1,69 @@
+import React from "react";
+
+import AudioButton from "./AudioButton";
+import { color } from "../assets/colors/colors";
+
+function TranslationsGrid({ data }) {
+  return (
+    <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
+      <div style={styles.row}>
+        <div className="card" style={styles.header}>
+          English
+        </div>
+        <div className="card" style={styles.header}>
+          Igbo
+        </div>
+        <div style={{ flex: 1 }}></div>
+      </div>
+      {data.map((vocab, index) => {
+        return <TranslationRow key={index} vocab={vocab} />;
+      })}
+    </div>
+  );
+}
+
+const TranslationRow = ({ vocab }) => {
+  return (
+    <div style={styles.row}>
+      <div className="card-ns" style={styles.wordCards}>
+        <span style={{ padding: "0 20px" }}>{vocab.english}</span>
+      </div>
+      <div className="card-ns" style={styles.wordCards}>
+        <span style={{ padding: "0 20px" }}>{vocab.igbo}</span>
+      </div>
+      <div className="card-ns" style={styles.audioBtn}>
+        <AudioButton url={vocab.sound} width="35px" fillSpace={true} />
+      </div>
+    </div>
+  );
+};
+const styles = {
+  header: {
+    flex: 4,
+    backgroundColor: color.tealtint,
+    height: "60px",
+    padding: "10px 0",
+    fontWeight: "bold",
+  },
+  row: {
+    display: "flex",
+    flexWrap: "wrap",
+    gap: "20px",
+  },
+  wordCards: {
+    flex: 4,
+    backgroundColor: color.white,
+    height: "60px",
+    padding: "10px 0",
+    alignItems: "start",
+  },
+  audioBtn: {
+    flex: 1,
+    backgroundColor: color.white,
+    height: "60px",
+    padding: "10px 0",
+    cursor: "pointer",
+  },
+};
+
+export default TranslationsGrid;
--- src/screens/vocab/PhrasebookExpanded.js
+++ src/screens/vocab/PhrasebookExpanded.js
@@ -4,10 +4,28 @@ import BottomColorStrip from "../../components/BottomColorStrip";
 import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
 import { icon } from "../../assets/images";
-import { color } from "../../assets/colors/colors";
 import { Spacer } from "../../components/utils";
-import AudioButton from "../../components/AudioButton";
 import sound from "../../assets/sound.wav";
+
+import TranslationsGrid from "../../components/TranslationsGrid";
+
+const data = [
+  { english: "Welcome", igbo: "nnọọ", sound: sound },
+  { english: "Good Morning", igbo: "ụtụtụ ọma", sound: sound },
+  { english: "Welcome", igbo: "nnọọ", sound: sound },
+  { english: "Good Morning", igbo: "ụtụtụ ọma", sound: sound },
+  { english: "Welcome", igbo: "nnọọ", sound: sound },
+  { english: "Good Morning", igbo: "ụtụtụ ọma", sound: sound },
+  { english: "Welcome", igbo: "nnọọ", sound: sound },
+  { english: "Good Morning", igbo: "ụtụtụ ọma", sound: sound },
+  { english: "Welcome", igbo: "nnọọ", sound: sound },
+  { english: "Good Morning", igbo: "ụtụtụ ọma", sound: sound },
+  { english: "Welcome", igbo: "nnọọ", sound: sound },
+  { english: "Good Morning", igbo: "ụtụtụ ọma", sound: sound },
+  { english: "Welcome", igbo: "nnọọ", sound: sound },
+  { english: "Good Morning", igbo: "ụtụtụ ọma", sound: sound },
+  { english: "Welcome", igbo: "nnọọ", sound: sound },
+];
 
 function PhrasebookExpanded() {
   const page = "vocab";
@@ -35,79 +53,7 @@ function PhrasebookExpanded() {
         <p className="subtitle balsamiq-ig">Family - 15 Phrases</p>
         <Spacer height={"20px"} />
 
-        <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
-          <div style={{ display: "flex", flexWrap: "wrap", gap: "20px" }}>
-            <div
-              className="card"
-              style={{
-                flex: 4,
-                backgroundColor: color.tealtint,
-                height: "auto",
-                padding: "10px 0",
-                fontWeight: "bold",
-              }}
-            >
-              English
-            </div>
-            <div
-              className="card"
-              style={{
-                flex: 4,
-                backgroundColor: color.tealtint,
-                height: "auto",
-                padding: "10px 0",
-                fontWeight: "bold",
-              }}
-            >
-              Igbo
-            </div>
-            <div style={{ flex: 1 }}></div>
-          </div>
-          <div
-            style={{
-              display: "flex",
-              flexWrap: "wrap",
-              gap: "20px",
-            }}
-          >
-            <div
-              className="card-ns"
-              style={{
-                flex: 4,
-                backgroundColor: color.white,
-                height: "auto",
-                padding: "10px 0",
-                alignItems: "start",
-              }}
-            >
-              <span style={{ padding: "0 20px" }}>hi</span>
-            </div>
-            <div
-              className="card-ns"
-              style={{
-                flex: 4,
-                backgroundColor: color.white,
-                height: "auto",
-                padding: "10px 0",
-                alignItems: "start",
-              }}
-            >
-              <span style={{ padding: "0 20px" }}>hi</span>
-            </div>
-            <div
-              className="card-ns"
-              style={{
-                flex: 1,
-                backgroundColor: color.white,
-                height: "fit-content",
-                padding: "10px 0",
-                cursor: "pointer",
-              }}
-            >
-              <AudioButton url={sound} width="35px" fillSpace={true} />
-            </div>
-          </div>
-        </div>
+        <TranslationsGrid data={data} />
       </div>
     </div>
   );
