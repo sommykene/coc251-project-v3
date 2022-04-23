a0ed7775aa54d6c7517a2bd768c77fdc09396544
sommykene <32955277+sommykene@users.noreply.github.com>
2022-04-23 21:03:29 +0100

    new scorecard component

---
+++ src/screens/practice/games/components/ScoreCard.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { color } from "../../../../assets/colors/colors";
+
+function ScoreCard({ score }) {
+  return (
+    <div
+      className="card-ns balsamiq-ig"
+      style={{
+        background: color.white,
+        maxWidth: "80%",
+        minWidth: "60%",
+        paddingLeft: "20px",
+        paddingRight: "20px",
+        height: "fit-content",
+      }}
+    >
+      <p className="balsamiq-ig pagetitle">Well Done</p>
+      <p className="balsamiq-ig pagetitle">You Earnt +{score}XP Points</p>
+    </div>
+  );
+}
+
+export default ScoreCard;
